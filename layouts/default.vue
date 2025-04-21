5b4856b2880ae04c5a4828be2b35846b576d3942
NanPar22 <Chaiyanan22102002@gmail.com>
2025-04-21 14:28:11 +0700

    forders

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <header>Header</header>
+    <slot />
+      <footer>Footer</footer>
+    </div>
+  </template>
+  
+  <style>
+  /* เพิ่มสไตล์ตามต้องการ */
+  </style>
